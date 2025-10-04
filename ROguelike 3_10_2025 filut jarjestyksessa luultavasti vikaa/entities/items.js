68da336689cb66e43963f9b68a0ef5563a21d955
zakker111 <zakker690@gmail.com>
2025-10-04 02:10:03 +0000

    feat(items): add veitsi weapon definition and fix switch_blade indentation

--- ROguelike 3_10_2025 filut jarjestyksessa luultavasti vikaa/entities/items.js
+++ ROguelike 3_10_2025 filut jarjestyksessa luultavasti vikaa/entities/items.js
@@ -62,13 +62,18 @@
       name: (mat) => `${mat} sword`,
       atkRange: { 1: [0.5, 2.4], 2: [1.2, 3.4], 3: [2.2, 4.0] } },
 
+    veitsi: { key: "veitsi", slot: "hand", twoHanded: false,
+      weight: 0.50,
+      name: (mat) => `${mat} sword`,
+      atkRange: { 1: [0.5, 2.4], 2: [1.2, 3.4], 3: [2.2, 4.0] } },
+
     axe: { key: "axe", slot: "hand", twoHanded: false,
       weight: 0.25,
       name: (mat) => `${mat} axe`,
       atkRange: { 1: [0.5, 2.4], 2: [1.2, 3.4], 3: [2.2, 4.0] },
       atkBonus: { 1: [0.0, 0.3], 2: [0.1, 0.5], 3: [0.2, 0.6] } },
 
-switch_blade: { key: "switch_blade", slot: "hand", twoHanded: false,
+    switch_blade: { key: "switch_blade", slot: "hand", twoHanded: false,
       weight: 0.12,
       name: (mat) => `${mat} switch blade`,
       atkRange: { 1: [0.6, 1.4], 2: [1.3, 2.6], 3: [2.0, 3.2] },
